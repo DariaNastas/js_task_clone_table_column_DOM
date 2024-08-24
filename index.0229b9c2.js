d5f15fd42fc25db59707d5f9ef361936fc92d24d
Daria Nastas <bycinska@gmail.com>
2024-08-24 14:16:56 +0300

    make build

---
+++ index.0229b9c2.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector("table").querySelectorAll("tr").forEach(function(e){var t=e.querySelectorAll("th, td");if(t.length>=2){var n=t[1].cloneNode(!0),o=t[t.length-1];e.insertBefore(n,o)}})});
+//# sourceMappingURL=index.0229b9c2.js.map
